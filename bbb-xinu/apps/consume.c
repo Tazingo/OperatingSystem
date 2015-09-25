2c39496f44228e96cfd3b87d405c2a3120b1cdba
Jinbao Xiao <jinxiao@indiana.edu>
2015-09-24 21:43:39 -0400

    assignment 2 update

--- bbb-xinu/apps/consume.c
+++ bbb-xinu/apps/consume.c
@@ -6,5 +6,10 @@ void consumer(int count)
 	//Code to consume values of global variable 'n' until the value
 	//of n is less than or equal to count
 	//print consumed value e.g. consumed : 8
-  printf("consumed: %d\n", n);
+  int i = 0;
+  while(i< 5)
+  {
+    printf("consumed: %d\n", n);
+    i++;
+  }
 }
--- bbb-xinu/apps/produce.c
+++ bbb-xinu/apps/produce.c
@@ -6,7 +6,11 @@ void producer(int count)
 	//Code to produce values less than equal to count, 
 	//produced value should get assigned to global variable 'n'.
 	//print produced value e.g. produced : 8
-  int number = rand() % count;
-  n = number;
-  printf("produced: %d\n", n);
+  int i = 0;
+  while(i< 5)
+  {
+    n = rand() % count;
+    printf("produced: %d\n", n);    
+    i++;
+  } 
 }
